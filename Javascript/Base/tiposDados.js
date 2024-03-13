f98843e80cee907849ec5494934b1e6711411cf5
Luisa Tavares <asiuly.tav@gmail.com>
2024-03-13 04:41:12 -0300

    tipos de dados 13/03 4:40

---
+++ Javascript/Base/tiposDados.js
@@ -0,0 +1,32 @@
+//tipos de dados mais complexos......
+
+//--------------------------------------------------------
+//Objeto : dado estrutural
+//possui propriedades/atributos que são atribuidos a eles
+// também possuem funcionalidades / métodos
+
+({
+    nome: "Luisa",
+    idade: 25,
+    andar: function (){
+        console.log("andando")
+    }
+})
+
+// acima, temos um objeto com duas propriedades/atributos e uma função
+//----------------------------------------------------------------
+
+//ARRAY / VETORES
+// uma lista que agrupa dados
+
+//item 0, item 1
+["Luisa", 36]
+
+console.log([
+    "Luisa",
+    36,
+    "seila",
+    "item3"
+])
+
+//--------------------------------------------------------
